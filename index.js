719e1b55022c3af9eef7cad991c2a6cd29cabe01
Luca Gandini <lgandini@funambol.com>
2021-03-02 16:41:22 +0100

    Removed unused file

--- index.js
+++
@@ -1,8 +0,0 @@
-exports.objectFilter = (object, filterFunction) => {
-  return Object.keys(object)
-    .filter((key) => filterFunction(key, object[key]))
-    .reduce((filteredObject, currentKey) => {
-      filteredObject[currentKey] = object[currentKey];
-      return filteredObject;
-    }, {});
-};
